4d327c7c7410a6d8dddd4d1357266be20cf8822f
Quoc Hung <hungb1910076@student.ctu.edu.vn>
2023-02-23 23:37:52 +0700

    "Cai dat xu ly loi"

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
@@ -8,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+app.use((req, res, next) => {return next(new ApiError(404, "Resource not found"));});
+app.use((error, req, res, next) => {return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",});
+    });
 app.get("/", (req, res) => {res.json({ message: "Welcome to contact book application." });});
 
 module.exports = app;
